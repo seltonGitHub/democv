34fdabdac67a23eb3faa17eec53dad8f5e13d406
Selton <seltonzyf@outlook.com>
2019-08-02 13:58:27 +0800

    enhancement #1 init.

---
+++ src/team/demo/opencv/TestIsOk.java
@@ -0,0 +1,9 @@
+package team.demo.opencv;
+
+import org.opencv.core.Core;
+
+public class TestIsOk {
+    public static void main(String[] args) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+}
